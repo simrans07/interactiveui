317d03200430597972ddc70c47d8897d9ca93979
simran s <sps022011@gmail.com>
2025-07-23 15:05:33 -0700

    programmed button actioin -> from what is your name to welcome, _

--- interactiveui/ContentView.swift
+++ interactiveui/ContentView.swift
@@ -8,16 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name = ""
+    @State private var textTitle = "What is your name?"
     var body: some View {
         VStack {
-            Text("What is your name?")
+            Text(textTitle)
                 .font(.title)
-            TextField("Type your name here", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+            TextField("Type your name here", text: $name)
                 .multilineTextAlignment(.center)
                 .border(Color.blue, width: 1)
                 .font(.callout)
             Button("submit name") {
-                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                textTitle = "Welcome, \(name)!"
             }//close button
             .buttonStyle(.borderedProminent)
             .tint(.gray)
