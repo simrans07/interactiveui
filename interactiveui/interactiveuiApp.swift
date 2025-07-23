7fe3ee16e928449f3f6c88ff0ea85d4b9dbbe478
simran s <sps022011@gmail.com>
2025-07-23 14:27:17 -0700

    Initial Commit

---
+++ interactiveui/interactiveuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  interactiveuiApp.swift
+//  interactiveui
+//
+//  Created by Scholar on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct interactiveuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
